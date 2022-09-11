45615b39b39a717d2e9820218f261e5dbebb9dbc
Yunes B <yunes.bakhush@gmail.com>
2022-09-11 13:40:50 +0600

    refactor: logic and ui adjusted

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,34 +1,40 @@
 export class Popup {
-    constructor (popupSelector) {
-        this._popup = document.getElementById(popupSelector);
-        this.handleEscClose = this._handleEscClose.bind(this);
-    }
+  constructor(popupSelector) {
+    this._popup = document.getElementById(popupSelector);
+    this.handleEscClose = this._handleEscClose.bind(this);
+    this._body = document.body;
+  }
 
-    open() {
-        this._popup.classList.add('popup_opened');
-        document.addEventListener('keyup', this.handleEscClose);
-    }
+  open() {
+    this._popup.classList.add("popup_opened");
+    document.addEventListener("keyup", this.handleEscClose);
+    this._body.style.overflow = "hidden";
+  }
 
-    close() {
-        this._popup.classList.remove('popup_opened');
-        document.removeEventListener('keyup', this.handleEscClose);
-    }
+  close() {
+    this._popup.classList.remove("popup_opened");
+    document.removeEventListener("keyup", this.handleEscClose);
+    this._body.style.overflow = "visible";
+  }
 
-    _handleEscClose(evt) {
-        if (evt.key === "Escape") {
-            this.close();
-        };
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
     }
+  }
 
-    setEventListeners() {
-        this._popup.addEventListener('click', (evt) => {
-            if (!evt.target.closest('.popup__container') && !evt.target.closest('.popup__img-container')) {
-                this.close();
-            }
-        });
-        
-        this._popup.querySelector('.popup__close').addEventListener('click', () => {
-            this.close();
-        });
-    }
+  setEventListeners() {
+    this._popup.addEventListener("click", (evt) => {
+      if (
+        !evt.target.closest(".popup__container") &&
+        !evt.target.closest(".popup__img-container")
+      ) {
+        this.close();
+      }
+    });
+
+    this._popup.querySelector(".popup__close").addEventListener("click", () => {
+      this.close();
+    });
+  }
 }
